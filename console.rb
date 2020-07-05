fd93627d2c66b5b7e53abf9a89638f4c66f8bf49
Rebeka Geczi <geczirebeka@gmail.com>
2020-07-05 10:58:33 +0100

    first select done

--- console.rb
+++ console.rb
@@ -10,13 +10,22 @@ Film.delete_all()
 Ticket.delete_all()
 
 customer1 = Customer.new({ 'name' => 'George Ramsey', 'funds' => 50})
+customer2 = Customer.new({ 'name' => 'Alex Coates', 'funds' => 40})
+customer3 = Customer.new({ 'name' => ''})
+
 customer1.save()
+customer2.save()
 
 film1 = Film.new({ 'title' => 'Troy', 'price' => 10})
+film2 = Film.new({ 'title' => 'Gladiator', 'price' => 7})
+
 film1.save()
+film2.save()
 
 ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket2 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film2.id})
 ticket1.save()
+ticket2.save()
 
 binding.pry
 nil
--- models/film.rb
+++ models/film.rb
@@ -46,5 +46,10 @@ class Film
         return result
     end
 
-
+    def customers()
+        sql = "SELECT customers.* FROM customers INNER JOIN tickets ON customers.id = tickets.customer_id WHERE film_id = $1"
+        values = [@id]
+        customer_data = SqlRunner.run(sql, values)
+        return Customer.map_items(customer_data)
+    end
 end
