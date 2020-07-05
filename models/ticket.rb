6e36d2ccd2c67b2863eebfe04ec468d58c3cd2df
Rebeka Geczi <geczirebeka@gmail.com>
2020-07-05 09:33:11 +0100

    classes set up with CRUD

---
+++ models/ticket.rb
@@ -0,0 +1,45 @@
+require_relative('../db/sql_runner')
+
+class Ticket
+
+    attr_reader :id
+    attr_accessor :film_id, :customer_id
+
+    def initialize(options)
+        @id = options['id'].to_i if options['id']
+        @customer_id = options['customer_id'].to_i
+        @film_id = options['film_id'].to_i
+    end
+
+    def save()
+        sql = "INSERT INTO tickets (film_id, customer_id) VALUES ($1, $2) RETURNING id"
+        values = [@film_id, @customer_id]
+        ticket = SqlRunner.run(sql, values).first
+        @id = ticket['id'].to_i
+    end
+
+    def update()
+        sql = "UPDATE tickets SET (film_id, customer_id) = ($1, $2) WHERE id = $3"
+        values = [@film_id, @customer_id, @id]
+        SqlRunner.run(sql, values)
+    end
+
+    def delete()
+        sql = "DELETE FROM tickets where id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
+    def self.all()
+        sql = "SELECT * FROM tickets"
+        data = SqlRunner.run(sql)
+        return data.map{ |ticket| Ticket.new(ticket)}
+    end
+
+    def self.delete_all()
+        sql = "DELETE FROM tickets"
+        SqlRunner.run(sql)
+    end
+
+
+end
